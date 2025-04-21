c4f4ce856352a033e699740133fbcc71edead6d8
gregsadetsky <lepetitg@gmail.com>
2025-04-21 18:21:52 -0400

    .

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 from textual_serve.server import Server
 
-server = Server("python -m textual")
+server = Server(
+    "python -m textual",
+    host="0.0.0.0",
+    port=8000,
+    public_url="https://textual.greg.technology",
+)
 server.serve()
